e48cea707f178bc40aa4da0f1b80df18c5e164b2
oscarxjauregui@gmail.com <19031369@itcelaya.edu.mx>
2025-05-27 21:44:24 -0600

    xd

--- test_stripe.cjs
+++
@@ -1,6 +0,0 @@
-const stripe = require("stripe")("sk_test_51RSvqBI8PAfLPUB0YaAdUa59IbB4D4XzMfsw12HbUSsvDOn90V64QX5g0qtKwISmZqJb9aRDaVYYbRYjOyR7trfD00xKSncO6G");
-
-(async () => {
-  const customers = await stripe.customers.list();
-  console.log(customers);
-})();
